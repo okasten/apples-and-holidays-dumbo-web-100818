6db0bb9cc4cb7e5259eee13be8850c26ddc7b4dc
Olivia Kasten <odkasten@gmail.com>
2018-09-24 20:18:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,7 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
       if day.to_s.include?("_")
         day = day.to_s
         day.gsub!("_", " ")
-        day.split!
+        day = day.split
         day.each do |word|
           word.capitalize!
         end 
