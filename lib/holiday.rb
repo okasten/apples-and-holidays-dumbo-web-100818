4400ba15d671fc2ebb9dabd1d81478fbb51b7547
Olivia Kasten <odkasten@gmail.com>
2018-09-24 19:54:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_supplies_in_holidays(holiday_hash)
         day = day.to_s
         day.gsub!("_", " ").titleize
       end 
-      puts "  #{day.to_s.capitalize}: #{supplies.join(", ")}"
+      puts "  #{day}: #{supplies.join(", ")}"
     end 
   end 
 end
